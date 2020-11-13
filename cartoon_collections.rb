d6d5d7901da258f2951aa9f7156aae559ea3c224
Annelise <annelzz26@gmail.com>
2020-11-13 14:42:29 -0800

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_names) 
+  count = 0
+  while count < dwarf_names.length
+    dwarf_names.each_with_index do |item, index|
+      puts "#{index + 1}, #{item}"
+    end
+    count += 1 
+    end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  x = 0
+  while x < planeteer_calls.length
+    planeteer_calls.collect do |other_item|
+      puts "{other_item.capitalize}!"
+      x += 1
+    end
+  end
 end
 
 def long_planeteer_calls# code an argument here
