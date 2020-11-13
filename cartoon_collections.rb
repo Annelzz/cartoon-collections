2b8b5e351ec4ac59bd5c409dfb0ba5d334afcc69
Annelise <annelzz26@gmail.com>
2020-11-13 15:33:18 -0800

    commit tree bf55515fe8069649ecdf3906d64680d6430b89c6

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,19 +10,28 @@ end
 
 def summon_captain_planet(planeteer_calls)
   x = 0
+  new_list = []
   while x < planeteer_calls.length
     planeteer_calls.collect do |other_item|
-      puts "{other_item.capitalize}!"
+      new_list << "#{other_item.capitalize}!"
       x += 1
     end
+    return new_list
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? do |letters|
+    letters.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(food)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  food.detect do |snack|
+  cheese_types.include? snack
+  end
 end
+
+
+  
